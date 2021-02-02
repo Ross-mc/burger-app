d40a8cd896c055d061a2fe2d1039d8533d442010
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-02-02 10:06:06 +0000

    Feat: added basic route

---
+++ controllers/burgerController.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const burger = require('../models/burger');
+
+const router = express.Router();
+
+//routes
+
+//default route
+router.get('/', (req, res) => {
+    const test = {
+        test: 'hello'
+    }
+    res.render("index", test)
+})
