143143e0b47901b526bf0c984d5878697a1d589d
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-02-02 10:01:51 +0000

    Feat: set up express server, added middleware and hbs engine

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const routes = require('./controllers/burgerController');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+//serve static content
+app.use(express.static('public'));
+
+//middleware
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//set up handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//pass routes to server
+// app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+})
+
